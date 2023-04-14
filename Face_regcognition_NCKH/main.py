f9c56bc46706982a6a7b64dab747aeeef7c19381
ThanhTung09 <102369873+tungkmt@users.noreply.github.com>
2023-04-14 10:44:32 +0700

    face-regconition

---
+++ Face_regcognition_NCKH/main.py
@@ -0,0 +1,12 @@
+print("helo")
+import pickle
+import numpy as np
+
+
+with open('dataface.dat', 'rb') as f:
+    all_face_encodings = pickle.load(f)
+    known_face_names = list(all_face_encodings.keys())
+    known_face_encodings = np.array(list(all_face_encodings.values()))
+    print(known_face_names)
+    print(known_face_encodings.shape)
+#
